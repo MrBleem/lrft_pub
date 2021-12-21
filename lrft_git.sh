cc8903f13e6067498a6c62329b4d8a25ad4ed113
MrBleem <1194189468@qq.com>
2021-12-21 07:39:45 -0500

    lrft_pub

--- lrft_git.sh
+++ lrft_git.sh
@@ -4,16 +4,16 @@ git config --global user.email "1194189468@qq.com"
 git config --global user.name "MrBleem"
 
 # echo "# lrft" >> README.md
-# version="v1.0.0"
+version="v1.0.1"
 
 git init
 git add .
 git commit -m "lrft_pub"
 git branch -M main
-# git tag -a ${version} -m "try the function of release"
+git tag -a ${version} -m "try the function of release"
 git remote add origin git@github.com:Project-XWH/lrft_pub.git
-# git push origin ${version}
-git pull origin main
-git push -u origin main
+git push origin ${version}
+# git pull origin v1
+# git push -u origin v1
 
 
